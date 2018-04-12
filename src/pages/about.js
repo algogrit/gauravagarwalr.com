3a058c9766eb3d82b103be0343f97aede6f97a57
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-04-13 02:52:10 +0530

    Grammar correction.

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,7 +53,7 @@ const AboutSection = () => (
         </p>
 
         <p>
-          I am a full stack developer, willing to get his hands dirty in order to get things moving. I don't believe in working in silos. I find it to be comforting to understand all layers of a stack.
+          I am a full stack developer, willing to get his hands dirty in order to get things moving. I don't believe in working in silos. I find it comforting to understand all layers of a stack.
         </p>
 
         <p>I am a freelance technical consultant. Hit me up and we can discuss how I can help you in bringing your next big idea to life.</p>
