3098939f07018035c0ff5b6c056252ab2c7fd448
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-04-13 20:23:29 +0530

    chef -> musician.

--- src/pages/interests.js
+++ src/pages/interests.js
@@ -5,7 +5,7 @@ const InterestsSection = () => (
     <div className="my-auto">
       <h2 className="mb-5">Interests</h2>
       <p>Apart from being a developer, I enjoy most of my time being outdoors. I like playing Table Tennis, Chess, Beach Volleyball and Soccer. Swimming is my favorite thing to do in summer.</p>
-      <p className="mb-0">Indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the development world.</p>
+      <p className="mb-0">Indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring musician, and I spend a large amount of my free time exploring the latest technolgy advancements in the development world.</p>
     </div>
   </section>
 )
